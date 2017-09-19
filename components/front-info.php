de435f0ab5ce8b4e9b05d743ddaf5d60ab8bff10
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2017-09-19 11:00:47 +0700

    separate file to component(s)

---
+++ components/front-info.php
@@ -0,0 +1,47 @@
+<section style="background-color: #0088D3; padding: 0;" id="course">
+    <div class="each-course">
+        <div class="col-group course-row">
+            <div class="col-12 col-dt-8">
+                <img class="course clip" src="<?php echo get_theme_file_uri('img/111.jpg'); ?>">
+            </div>
+            <div class="col-12 col-dt-4 course-text">
+                <div class="content-course">
+                    <h2>หลักสูตรปริญญาตรี</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna</p>
+                    <a href="#" class="button round inverted outline">ดูเพิ่มเติม</a>
+                </div>
+            </div>
+        </div><!-- .col-group .course-row -->
+    </div><!-- .each-course -->
+    <div class="each-course">
+        <div class="col-group course-row">
+            <div class="col-12 col-dt-8 push-last">
+                <img class="course clip clip-left" src="<?php echo get_theme_file_uri('img/222.jpg'); ?>">
+            </div>
+            <div class="col-12 col-dt-4 course-text push-first">
+                <div class="content-course left">
+                    <h2>หลักสูตรปริญญาตรี</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna</p>
+                    <a href="#" class="button round inverted outline">ดูเพิ่มเติม</a>
+                </div>
+            </div>
+        </div><!-- .col-group .course-row -->
+    </div><!-- .each-course -->
+    <div class="each-course">
+        <div class="col-group course-row">
+            <div class="col-12 col-dt-8">
+                <img class="course clip" src="<?php echo get_theme_file_uri('img/333.jpg'); ?>">
+            </div>
+            <div class="col-12 col-dt-4 course-text">
+                <div class="content-course">
+                    <h2>หลักสูตรปริญญาตรี</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna</p>
+                    <a href="#" class="button round inverted outline">ดูเพิ่มเติม</a>
+                </div>
+            </div>
+        </div><!-- .col-group .course-row -->
+    </div><!-- .each-course -->
+</section>
---
+++ components/front-news.php
@@ -0,0 +1,34 @@
+<section style="background-color: #d7d7d7" id="home-news-section">
+    <h1 class="title">ข่าวสารและกิจกรรม</h1>
+    <div class="owl-carousel" id="home-news">
+
+    <?php
+    $the_query = new WP_Query(array(
+        'posts_per_page' => 5,
+        'category_name' => 'news',
+    ));
+    if ($the_query->have_posts()) :
+        while ($the_query->have_posts()) : $the_query->the_post();
+    ?>
+
+        <div class="card news-card">
+            <?php if(has_post_thumbnail()): ?>
+                <?php the_post_thumbnail( 'full', array('class' => 'featured-image', 'id' => 'main-feat-image')); ?>
+            <?php endif; ?>
+            <div class="padded">
+                <h3><?php the_title(); ?></h3>
+                <p><?php the_content('', FALSE, ''); ?></p>
+                <a href="<?php echo get_the_permalink() ?>" class="button round">ดูเพิ่มเติม</a>                
+            </div>
+        </div>
+
+    <?php
+        endwhile;
+        endif
+    ?>
+
+    </div>
+    <div style="text-align: center; margin: 2em 1em 1em">
+        <a href="#" class="button outline">ข่าวสารและกิจกรรมทั้งหมด</a>
+    </div>
+</section>
