693ace6bc1a77112ccd5ef19547ebbfbee415c30
Nathan Yiangsupapaanontr <zartre@Nathans-MacBook-Pro.local>
2017-10-12 22:32:30 +0700

    Add Courses page

---
+++ courses-page.php
@@ -0,0 +1,62 @@
+<?php /*
+Template Name: Research page
+Template Post Type: page
+*/
+
+get_header();
+
+if (have_posts()) :
+	while (have_posts()) : the_post(); ?>
+		<section class="page page--courses">
+			<div class="page-content">
+				<div class="page-hero">
+					<h1 class="text-center">หลักสูตร</h1>
+					<h3 class="text-center">ผลิตบัณฑิตที่พร้อมด้วยคุณสมบัติอันพึงประสงค์ ในสาขาวิชาที่สอดคล้องกับความต้องการของสังคม</h3>
+				</div>
+				<section class="all-courses">
+					<div class="container">
+						<p class="text-center" id="course-overview">หลักสูตรที่มุ่งพัฒนาผู้เรียน ให้มีความรอบรู้อย่างกว้างขวาง มีโลกทัศน์ที่กว้างไกล มีความเข้าใจตนเอง ผู้อื่น และสังคม</p>
+						<div class="col-group">
+							<div class="col-12">
+								<div class="bg-box" style="background-image: url(<?php echo get_theme_file_uri('img/course-1.jpg'); ?>);">
+									<h3>หลักสูตรปริญญาตรี</h3>
+									<p>หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาเทคโนโลยีสารสนเทศ<br/>
+									(Bachelor of Science Program in Information Technology)</p>
+									<div class="btn-wrap">
+										<a href="#" class="button outline inverted">สาขาวิชาเทคโนโลยีสารสนเทศ</a>
+									</div>
+									<div class="btn-wrap">
+										<a href="#" class="button outline inverted">สาขาวิชาวิทยาการข้อมูลและการวิเคราะห์เชิงธุรกิจ</a>
+									</div>
+									<div class="btn-wrap">
+										<a href="#" class="button outline inverted">สาขาเทคโนโลยีสารสนเทศทางธุรกิจ (หลักสูตรนานาชาติ)</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="box-wrap">
+							<div class="bg-box" style="background-image: url(<?php echo get_theme_file_uri('img/course-2.jpg'); ?>);">
+								<h3>หลักสูตรปริญญาโท</h3>
+								<p>หลักสูตรวิทยาศาสตรมหาบัณฑิต สาขาวิชาเทคโนโลยีสารสนเทศ<br/>
+								(Master of Science Program in Information Technology)</p>
+								<a href="#" class="button outline inverted">รายละเอียด</a>
+							</div>
+							<div class="bg-box" style="background-image: url(<?php echo get_theme_file_uri('img/course-3.jpg'); ?>);">
+								<h3>หลักสูตรปริญญาเอก</h3>
+								<p>หลักสูตรปรัชญาดุษฎีบัณฑิต สาขาวิชาเทคโนโลยีสารสนเทศ<br/>
+								(Doctor of Philosophy Program in Information Technology)</p>
+								<a href="#" class="button outline inverted">รายละเอียด</a>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div><!-- .page-content -->
+		</section><!-- section.page.page-research -->
+<?php
+
+endwhile;
+endif;
+
+get_footer();
+
+?>
