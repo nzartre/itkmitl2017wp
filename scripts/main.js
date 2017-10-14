2e03a7a74c5f54101e91a387b804b84453b8cc78
Nathan Yiangsupapaanontr <zartre@Nathans-MacBook-Pro.local>
2017-10-14 16:40:02 +0700

    Create video element in if-condition

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
-var heroVideo=document.createElement("source");heroVideo.setAttribute("src","./img/itkmitl-h264.mp4"),heroVideo.setAttribute("type","video/mp4"),window.innerWidth>768&&document.getElementById("hero-video").appendChild(heroVideo),$("#main-feat-image").height()>$("#main-feat-image").width()&&$("#main-feat-image").style("max-width","400px"),$(".nav-chevron").click(function(){$(".mobile-nav").toggle()}),$(document).ready(function(){$("#home-news").owlCarousel({center:!0,items:1,margin:10,loop:0,lazyLoad:1,dots:1,responsive:{720:{items:3,nav:0},1280:{items:4}}})});
-//# sourceMappingURL=main.js.map
+if (window.innerWidth > 768) {
+    var heroVideo = document.createElement('source');
+    heroVideo.setAttribute('src', './img/itkmitl-h264.mp4');
+    heroVideo.setAttribute('type', 'video/mp4');
+    document.getElementById('hero-video').appendChild(heroVideo);
+}
+
+if ($('#main-feat-image').height() > $('#main-feat-image').width()) {
+    $('#main-feat-image').style('max-width', '400px');
+}
+
+$('.nav-chevron').click(function(){
+    $('.mobile-nav').toggle();
+});
+
+$(document).ready(function() {
+    $('#home-news').owlCarousel({
+        center: true,
+        items: 1,
+        margin: 10,
+        loop: 0,
+        lazyLoad: 1,
+        dots: 1,
+        responsive: {
+            720: {
+                items: 3,
+                nav: 0
+            },
+            1280: {
+            	items: 4
+            }
+        }
+    });
+});
