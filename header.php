386b6ffffb361f1166914e99fbe97f2a0cc9ced5
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2017-10-19 21:17:02 +0700

    add dynamic title bar

--- header.php
+++ header.php
@@ -6,9 +6,6 @@
     <meta name='viewport' content='width=device-width'>
     <!-- Welcome you naughty coder! Unleash your inner curiosity and dig through our website. -->
     <!-- BTW, check out our courses because people like you should find it happy to study here! -->
-    <title>
-        <?php bloginfo( 'name' ); ?>
-    </title>
     <?php wp_head(); ?>
     <?php if (is_front_page()) {
         $owl_carousel = get_theme_file_uri('vendor/owl/assets/owl-theme.min.css');
