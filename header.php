4d69877d3674bff04025e1d14eed8f042cd5f412
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-10-20 20:38:41 +0700

    Move OG above wp_head

--- header.php
+++ header.php
@@ -6,13 +6,13 @@
     <meta name='viewport' content='width=device-width'>
     <!-- Welcome you naughty coder! Unleash your inner curiosity and dig through our website. -->
     <!-- BTW, check out our courses because people like you should find it happy to study here! -->
-    <?php wp_head(); ?>
     <meta property="og:url" content="<?php home_url(add_query_arg( array(), $wp->request )); ?>">
     <meta property="og:type" content="article">
     <meta property="og:image" content="<?php echo get_theme_file_uri('img/ogimage.png'); ?>">
     <meta property="og:video" content="<?php echo get_theme_file_uri('img/itkmitl-h264.mp4'); ?>">
     <meta property="og:locale" content="<?php echo get_locale() ?>">
     <meta property="article:publisher" content="https://www.facebook.com/ITLadkrabang/">
+    <?php wp_head(); ?>
     <?php if (is_front_page()) {
         $owl_carousel = get_theme_file_uri('vendor/owl/assets/owl-theme.min.css');
         $owl_theme = get_theme_file_uri('vendor/owl/assets/owl.carousel.min.css');
