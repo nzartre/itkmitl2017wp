cea0fbba70736f6c2d86b21edb0210684371f184
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-10-21 11:57:53 +0700

    Don't insert hero video on Edge and Firefox

--- front-page.php
+++ front-page.php
@@ -11,27 +11,25 @@ get_footer();
 
 <script>
 	if (window.innerWidth > 768) {
-		var heroVideo = document.createElement('source');
-		heroVideo.setAttribute('src', '<?php echo get_theme_file_uri('img/itkmitl-h264.mp4'); ?>');
-		heroVideo.setAttribute('type', 'video/mp4');
-		document.getElementById('hero-video').appendChild(heroVideo);
-	}
-	jQuery(document).ready(function( $ ) {
+        var browser = detectBrowser();
+        if (browser.name != "edge" && browser.name != 'firefox') {
+            var heroVideo = document.createElement('source');
+            heroVideo.setAttribute('src', 'img/itkmitl-h264.mp4');
+            heroVideo.setAttribute('type', 'video/mp4');
+            document.getElementById('hero-video').appendChild(heroVideo);
+        }
+    }
+    jQuery(document).ready(function( $ ) {
         $('#home-news').owlCarousel({
             center: true,
             items: 1,
             margin: 10,
-            loop: 0,
+            loop: 1,
             lazyLoad: 1,
             dots: 1,
             responsive: {
-                720: {
-                    items: 3,
-                    nav: 0
-                },
-                1280: {
-                    items: 4
-                }
+                720: {items: 3,nav: 0},
+                1280: {items: 4}
             }
         });
     });
