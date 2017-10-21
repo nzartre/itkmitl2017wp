971a21cc7a64e2a8a2fcc3116ecd9a0dd9f66307
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-10-21 12:05:34 +0700

    Fix video URL

--- front-page.php
+++ front-page.php
@@ -9,12 +9,13 @@ get_template_part( 'components/front', 'info' );
 get_footer();
 ?>
 
+<script src="<?php echo get_theme_file_uri('scripts/detectbrowser.js'); ?>"></script>
 <script>
 	if (window.innerWidth > 768) {
         var browser = detectBrowser();
         if (browser.name != "edge" && browser.name != 'firefox') {
             var heroVideo = document.createElement('source');
-            heroVideo.setAttribute('src', 'img/itkmitl-h264.mp4');
+            heroVideo.setAttribute('src', '<?php echo get_theme_file_uri('img/itkmitl-h264.mp4'); ?>');
             heroVideo.setAttribute('type', 'video/mp4');
             document.getElementById('hero-video').appendChild(heroVideo);
         }
