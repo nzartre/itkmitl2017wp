47de83cc4cae1b5f3a76db982dae0eb5d071a02e
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-10-21 14:13:23 +0700

    Use featured image as ogimage

--- header.php
+++ header.php
@@ -8,9 +8,11 @@
     <!-- BTW, check out our courses because people like you should find it happy to study here! -->
     <meta property="og:url" content="<?php home_url(add_query_arg( array(), $wp->request )); ?>">
     <meta property="og:type" content="article">
-    <meta property="og:image" content="<?php echo get_theme_file_uri('img/ogimage.png'); ?>">
+    <?php if(has_post_thumbnail()) $ogimage = get_the_post_thumbnail_url(); else $ogimage = get_theme_file_uri('img/ogimage.png'); ?>
+    <meta property="og:image" content="<?php echo $ogimage; ?>">
     <meta property="og:video" content="<?php echo get_theme_file_uri('img/itkmitl-h264.mp4'); ?>">
     <meta property="og:locale" content="<?php echo get_locale() ?>">
+    <meta property="og:site_name" content="คณะเทคโนโลยีสารสนเทศ พระจอมเกล้าลาดกระบัง">
     <meta property="article:publisher" content="https://www.facebook.com/ITLadkrabang/">
     <?php wp_head(); ?>
     <?php if (is_front_page()) {
