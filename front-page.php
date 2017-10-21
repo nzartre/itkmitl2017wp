4acbc75ab46f914b71db3087f583e89d83efd68c
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-10-21 13:34:50 +0700

    Use array for unsupported browsers

--- front-page.php
+++ front-page.php
@@ -13,7 +13,8 @@ get_footer();
 <script>
 	if (window.innerWidth > 768) {
         var browser = detectBrowser();
-        if (browser.name != "edge" && browser.name != 'firefox') {
+        var browsers = ['ie', 'edge', 'firefox'];
+        if (browsers.indexOf(browser.name) == -1) {
             var heroVideo = document.createElement('source');
             heroVideo.setAttribute('src', '<?php echo get_theme_file_uri('img/itkmitl-h264.mp4'); ?>');
             heroVideo.setAttribute('type', 'video/mp4');
