5ffbcbad45d79d0bab818c121666aacf3a0e48d1
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-11-01 21:13:22 +0700

    Change index banner text

--- components/front-info.php
+++ components/front-info.php
@@ -7,6 +7,7 @@
             <div class="col-12 col-dt-4 course-text">
                 <div class="content-course" style="margin:auto;">
                     <h2>หลักสูตรทั้งหมด</h2>
+                    <p>ผลิตบัณฑิตที่พร้อมด้วยคุณสมบัติอันพึงประสงค์ ในสาขาวิชาที่สอดคล้องกับความต้องการของสังคม ทั้งปริญญาตรี ปริญญาโท และปริญญาเอก</p>
                     <a href="#" class="button round inverted outline">ดูเพิ่มเติม</a>
                 </div>
             </div>
@@ -20,6 +21,7 @@
             <div class="col-12 col-dt-4 course-text push-first">
                 <div class="content-course left" style="margin:auto;">
                     <h2>การวิจัย</h2>
+                    <p>คณะสนับสนุนการวิจัยด้วยห้องปฏิบัติการ 3 กลุ่ม ทั้งกลุ่มวิทยาการสารสนเทศ กลุ่มระบบเครือข่าย และกลุ่มสื่อประสม</p>
                     <a href="#" class="button round inverted outline">ดูเพิ่มเติม</a>
                 </div>
             </div>
@@ -33,6 +35,7 @@
             <div class="col-12 col-dt-4 course-text">
                 <div class="content-course" style="margin:auto;">
                     <h2>Life@IT</h2>
+                    <p>บรรยากาศการเรียนและชีวิตประจำวันในคณะ</p>
                     <a href="#" class="button round inverted outline">ดูเพิ่มเติม</a>
                 </div>
             </div>
