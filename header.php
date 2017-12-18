953c7c2a66d6df59b43e06c3970ae0d802718956
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-12-18 09:39:08 +0700

    Add Google Analytics

--- header.php
+++ header.php
@@ -47,6 +47,15 @@
     ?>
     
     </title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90533167-1"></script>
+    <!--script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-90533167-1');
+    </script-->
 </head>
 
 <body <?php body_class(); ?>>
