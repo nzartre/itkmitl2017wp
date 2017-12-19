c95b240b4036137e1c998a3e607c89774573266a
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-12-19 21:59:16 +0700

    Add Staff and Publications to search result (not working)

--- functions.php
+++ functions.php
@@ -1,47 +1,57 @@
 <?php
 // Theme Support
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support('post-thumbnails');
 
 // Registration Section
 function home_inc() {
-    wp_enqueue_style( 'kube', get_template_directory_uri().'/styles/kube.css');    
-    wp_enqueue_style( 'fluidable', get_template_directory_uri().'/styles/fluidable.css');
-    wp_enqueue_style( 'style', get_stylesheet_uri());
+    wp_enqueue_style('kube', get_template_directory_uri() . '/styles/kube.css');
+    wp_enqueue_style('fluidable', get_template_directory_uri() . '/styles/fluidable.css');
+    wp_enqueue_style('style', get_stylesheet_uri());
 
-    wp_enqueue_script( 'main-js', get_template_directory_uri().'/scripts/main.js', array('jquery'), null, true );
-    wp_enqueue_script( 'kube-js', get_template_directory_uri().'/vendor/kube.min.js', array('jquery'), null, true );
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/scripts/main.js', array('jquery'), null, true);
+    wp_enqueue_script('kube-js', get_template_directory_uri() . '/vendor/kube.min.js', array('jquery'), null, true);
 }
 
-add_action( 'wp_enqueue_scripts', 'home_inc' );
+add_action('wp_enqueue_scripts', 'home_inc');
 
 function nav_loader() {
     register_nav_menus(
-            array(
-              'primary' => __( 'Top Fixed Menu' ),
-              'header' => __( 'Header Menu' ),
-              'inpage' => __( 'In-Page Menu' )
-            )
-        );
+        array(
+            'primary' => __('Top Fixed Menu'),
+            'header' => __('Header Menu'),
+            'inpage' => __('In-Page Menu')
+        )
+    );
 }
-add_action( 'init', 'nav_loader' );
+
+add_action('init', 'nav_loader');
 
 // Post Helper function
 function this_excerpt($limit) {
     return wp_trim_words(get_the_content(), $limit);
 }
 
-function wpdocs_excerpt_more( $more ) {
+function wpdocs_excerpt_more($more) {
     return '...';
 }
-add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
+
+add_filter('excerpt_more', 'wpdocs_excerpt_more');
 
 function itkmitl_redirect_post() {
-    if ( is_single() && 'staff' ==  get_query_var('post_type') ) {
-        wp_redirect( get_post_type_archive_link('staff'), 301 );
+    if (is_single() && 'staff' == get_query_var('post_type')) {
+        wp_redirect(get_post_type_archive_link('staff'), 301);
         exit;
     }
 }
-add_action( 'template_redirect', 'itkmitl_redirect_post' );
+
+add_action('template_redirect', 'itkmitl_redirect_post');
+
+function itkmitl_cpt_search($query) {
+    if ($query->is_search) $query->set('post_type', array('post', 'page', 'staff', 'publications'));
+    return $query;
+}
+
+add_filter('pre_get_posts', 'itkmitl_cpt_search');
 
 // Translation Section
 if (function_exists('pll_register_string')) {
