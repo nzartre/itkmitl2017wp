5586e2c269375432411cda49048fce1304bc0d9d
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-12-19 11:01:35 +0700

    Include only pages in Sitemap

--- sitemap.php
+++ sitemap.php
@@ -1,47 +1,34 @@
-<?php 
+<?php
 /*
 Template Name: Sitemap Page
+Template Post Type: page
 */
+
+get_header();
 ?>
-<h2>Authors</h2>
-<ul>
-<?php
-wp_list_authors(
-array(
-'exclude_admin' => false,
-)
-);
-?>
-</ul>
-<h2>Pages</h2>
-<ul>
-<?php
-// Add pages you'd like to exclude in the exclude here
-wp_list_pages(
-array(
-'exclude' => '',
-'title_li' => '',
-)
-);
-?>
-</ul>
-<h2>Posts</h2>
-<ul>
-<?php
-// Add categories you'd like to exclude in the exclude here
-$learnedia_cats = get_categories('exclude=');
-foreach ($learnedia_cats as $learnedia_cat) {
-$learnedia_posts_by_slug = array(
-'category_name'    => $learnedia_cat->slug
-);
-$learnedia_posts_array = get_posts( $learnedia_posts_by_slug );
-echo "<li><h3>".$learnedia_cat->cat_name."</h3>";
-echo "<ul>";
-foreach ($learnedia_posts_array as $learnedia_post){
-echo '<li><a href="'.get_permalink($learnedia_post).'">'.get_the_title($learnedia_post).'</a></li>';
-}
-echo "</ul>";
-echo "</li>";
-}
-?>
-</ul>
+
+<section class="page">
+    <div class="page-content">
+        <div class="paper">
+            <article class="container">
+                <h1 class="page-title">Sitemap</h1>
+                <ul>
+                    <?php
+                    $smp = get_page_by_title( 'Sitemap' )->ID;
+                    wp_list_pages(
+                        array(
+                            'exclude' => $smp,
+                            'title_li' => '',
+                        )
+                    );
+                    ?>
+                    <li><a href="staff"><?php pll_e('Staff'); ?></a></li>
+                    <li><a href="publications"><?php pll_e('Publications'); ?></a></li>
+                    <li><a href="Sitemap">Sitemap</a></li>
+                </ul>
+            </article><!-- .container -->
+        </div><!-- .paper -->
+    </div><!-- .page-content -->
+</section><!-- section.page -->
+
+<?php get_footer(); ?>
