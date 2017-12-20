14a4cafe1228e58d50b6de12d9c5314afac11004
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-12-20 14:07:29 +0700

    Change front-detail icons and update text

--- components/front-detail.php
+++ components/front-detail.php
@@ -4,7 +4,7 @@
             <div class="col-12 col-dt-4">
                 <div class="card">
                     <div class="padded">
-                        <img src="<?php echo get_theme_file_uri('img/card-icon.png'); ?>" class="icon" alt="">
+                        <img src="<?php echo get_theme_file_uri('img/card-icon-1.png'); ?>" class="icon" alt="">
                         <h3>หลักสูตรทั้งหมด</h3>
                         <p>รายละเอียดหลักสูตรของคณะเทคโนโลยีสารสนเทศทั้งปริญญาตรี, ปริญญาโท และ ปริญญาเอก</p>
                         <a href="program" class="button round">ดูเพิ่มเติม</a>
@@ -14,7 +14,7 @@
             <div class="col-12 col-dt-4">
                 <div class="card">
                     <div class="padded">
-                        <img src="<?php echo get_theme_file_uri('img/card-icon.png'); ?>" class="icon" alt="">
+                        <img src="<?php echo get_theme_file_uri('img/card-icon-2.png'); ?>" class="icon" alt="">
                         <h3>หลักสูตรอบรม</h3>
                         <p>โครงการอบรมความรู้ทางเทคโนโลยีสารสนเทศให้แก่สังคมเพื่อความพัฒนาคุณภาพสังคมอย่างยั่งยืน</p>
                         <a href="acad-services" class="button round">ดูเพิ่มเติม</a>
@@ -24,9 +24,9 @@
             <div class="col-12 col-dt-4">
                 <div class="card">
                     <div class="padded">
-                        <img src="<?php echo get_theme_file_uri('img/card-icon.png'); ?>" class="icon" alt="">
+                        <img src="<?php echo get_theme_file_uri('img/card-icon-3.png'); ?>" class="icon" alt="">
                         <h3>ที่ตั้งและการเดินทาง</h3>
-                        <p>สถานที่ตั้งและข้อมูลแผนที่การเดินทางมาเพื่อติดต่อคณะเทคโนโลยีสารสนเทศ IT@KMITL</p>
+                        <p>ช่องทางติดต่อ ที่ตั้ง และข้อมูลการเดินทางมายังคณะเทคโนโลยีสารสนเทศ พระจอมเกล้าฯ ลาดกระบัง</p>
                         <a href="about/travel" class="button round">ดูเพิ่มเติม</a>
                     </div>
                 </div>
