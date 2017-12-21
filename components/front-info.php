96fa95ee9b7e46f5b0104cb393284e258cdc3a50
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-12-21 12:06:29 +0700

    Update translations

--- components/front-info.php
+++ components/front-info.php
@@ -7,7 +7,7 @@
             <div class="banner-content" style="margin:auto;">
                 <h2>หลักสูตรทั้งหมด</h2>
                 <p>ผลิตบัณฑิตที่พร้อมด้วยคุณสมบัติอันพึงประสงค์ ในสาขาวิชาที่สอดคล้องกับความต้องการของสังคม ทั้งปริญญาตรี ปริญญาโท และปริญญาเอก</p>
-                <a href="program" class="button round inverted outline">ดูเพิ่มเติม</a>
+                <a href="program" class="button round inverted outline"><?php pll_e('Read more'); ?></a>
             </div>
         </div>
     </div><!-- .col-group .banner-row -->
@@ -19,7 +19,7 @@
             <div class="banner-content left" style="margin:auto;">
                 <h2>การวิจัย</h2>
                 <p>คณะสนับสนุนการวิจัยด้วยห้องปฏิบัติการ 3 กลุ่ม ทั้งกลุ่มวิทยาการสารสนเทศ กลุ่มระบบเครือข่าย และกลุ่มสื่อประสม</p>
-                <a href="research" class="button round inverted outline">ดูเพิ่มเติม</a>
+                <a href="research" class="button round inverted outline"><?php pll_e('Read more'); ?></a>
             </div>
         </div>
     </div><!-- .col-group .banner-row -->
@@ -31,7 +31,7 @@
             <div class="banner-content" style="margin:auto;">
                 <h2>Life@IT</h2>
                 <p>บรรยากาศสถานที่ การเรียน และชีวิตประจำวันในคณะ</p>
-                <a href="about/life" class="button round inverted outline">ดูเพิ่มเติม</a>
+                <a href="about/life" class="button round inverted outline"><?php pll_e('Read more'); ?></a>
             </div>
         </div>
     </div><!-- .col-group .banner-row -->
