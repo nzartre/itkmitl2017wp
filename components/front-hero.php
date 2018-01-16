eae799cd681ed509d70599aaea430369c0c23bc7
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-01-16 21:41:22 +0700

    Adjust hero spacing on mobile

--- components/front-hero.php
+++ components/front-hero.php
@@ -1,30 +1,33 @@
-<div class="hero">
-    <div class="site-name text-center">
-        <div id="hero-title-wrap">
-            <h1 id="hero-title"><?php pll_e('hero_faculty-IT'); ?></h1>
+<div id="hero-wrap">
+    <div id="spacer-top"></div>
+    <div class="hero">
+        <div class="site-name text-center">
+            <div id="hero-title-wrap">
+                <h1 id="hero-title"><?php pll_e('hero_faculty-IT'); ?></h1>
+            </div>
+            <div id="hero-subtitle-wrap">
+                <h2 id="hero-subtitle"><?php pll_e('hero_KMITL'); ?></h2>
+            </div>
         </div>
-        <div id="hero-subtitle-wrap">
-            <h2 id="hero-subtitle"><?php pll_e('hero_KMITL'); ?></h2>
+        <div id="scroll-wrap">
+            <img src="<?php echo get_theme_file_uri('img/scroll.png'); ?>" id="scroll-icon" width="32" height="32">
+            <span id="scroll-text"><?php pll_e('hero_scroll'); ?></span>
         </div>
-    </div>
-    <div id="scroll-wrap">
-        <img src="<?php echo get_theme_file_uri('img/scroll.png'); ?>" id="scroll-icon" width="32" height="32">
-        <span id="scroll-text"><?php pll_e('hero_scroll'); ?></span>
-    </div>
-    <div class="overlay"></div>
-    <video autoplay loop id="hero-video"></video>
-    <div class="container">
-        <div class="secondary-nav" style="display: none">
-            <?php
-                $menuParameters = array(
-                    'container'       => false,
-                    'echo'            => false,
-                    'items_wrap'      => '%3$s',
-                    'depth'           => 0,
-                    'theme_location'  => 'header',
-                );
-                echo strip_tags(wp_nav_menu( $menuParameters ), '<a>' );
-            ?>
+        <div class="overlay"></div>
+        <video autoplay loop id="hero-video"></video>
+        <div class="container">
+            <div class="secondary-nav" style="display: none">
+                <?php
+                    $menuParameters = array(
+                        'container'       => false,
+                        'echo'            => false,
+                        'items_wrap'      => '%3$s',
+                        'depth'           => 0,
+                        'theme_location'  => 'header',
+                    );
+                    echo strip_tags(wp_nav_menu( $menuParameters ), '<a>' );
+                ?>
+            </div>
         </div>
     </div>
 </div>
