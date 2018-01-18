53bef06680d5cbbe2f516903f7b1bf774382cc92
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2018-01-18 18:17:28 +0700

    Add Staff single template

---
+++ single-staff.php
@@ -0,0 +1,55 @@
+<?php
+
+get_header();
+
+if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+
+        <section class="single">
+            <div class="page-content">
+                <div class="container">
+                    <h3 style="margin-top: 2em"><?php the_title(); ?></h3>
+                    <div class="col-group">
+                        <div class="col-12 col-dt-9">
+                            <?php $meta = get_post_meta($post->ID); ?>
+                            <table class="stack-on-tl" style="margin-top: 1em">
+                                <tr>
+                                    <th>ตำแหน่ง</th>
+                                    <td><?php echo $meta['role'][0]; ?></td>
+                                </tr>
+                                <tr>
+                                    <th>โทรศัพท์</th>
+                                    <td><?php echo $meta['phone'][0]; ?></td>
+                                </tr>
+                                <tr>
+                                    <th>ห้อง</th>
+                                    <td><?php echo $meta['lab'][0]; ?></td>
+                                </tr>
+                                <tr>
+                                    <th>อีเมล</th>
+                                    <td><?php echo $meta['email'][0]; ?></td>
+                                </tr>
+                                <tr>
+                                    <th>เว็บไซต์</th>
+                                    <td><?php echo $meta['website'][0]; ?></td>
+                                </tr>
+                                <tr>
+                                    <th>LinkedIn</th>
+                                    <td><?php echo $meta['linkedin'][0]; ?></td>
+                                </tr>
+                            </table>
+                            <?php the_content(); ?>
+                        </div><!-- post content -->
+                    </div><!-- .col-group -->
+                </div><!-- .container -->
+            </div><!-- .page-content -->
+        </section>
+
+    <?php
+
+    endwhile;
+endif;
+
+get_footer();
+
+?>
