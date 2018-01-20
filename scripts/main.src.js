813c54527be42df02b75e5bd4bd52621845622b3
Foofy <foozaasksksk@gmail.com>
2018-01-20 08:29:07 +0700

    Revert "test"
    This reverts commit 496c1b007b4b0af495f386c6c899dd28b2377d3e.

--- scripts/main.src.js
+++ scripts/main.src.js
@@ -1,1 +1,26 @@
-ดฟดหกด
+jQuery(document).ready(function ($) {
+
+    if ($('#main-feat-image').height() > $('#main-feat-image').width()) {
+        $('#main-feat-image').style('max-width', '400px');
+    }
+    
+    $('.nav-chevron').click(function (){
+        $('.mobile-nav').toggle();
+    });
+
+    window.onload = function () {
+        if ($("#splash-modal").attr('splash') == "on") {
+            jQuery('button[data-target=#splash-modal]').trigger('click');        
+        }
+    };
+
+    $('html').click(function (e) {
+        if (e.target.id != 'lang-switch' && e.target.id != 'lang-switch-caret') $('#lang-list').removeClass('show');
+        else $('#lang-list').toggleClass('show');
+    });
+
+});
+console.log("%cOoooooooh!", "color: blue; font-size: xx-large; font-weight: bold");
+console.log("%cWanna sneak into our code? Feel free!", "color: blue; font-size: x-large;");
+console.log("%cIT KMITL web developer", "color: orange; font-size: large");
+console.log("lelelelelelelelelelelelelelelel");
