cdbc3265184ffbb1b966e2dd3f46f54255635921
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2018-10-25 21:19:49 +0700

    set maximum upload

--- functions.php
+++ functions.php
@@ -75,4 +75,8 @@ include('customizer.php');
 // TGM Plugin Activation
 require_once('tgmpa/itkmitl-plugins.php');
 
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
+
 ?>
